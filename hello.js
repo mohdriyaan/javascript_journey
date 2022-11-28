a580df98faedd83dc7418b521b7ae71ae4cdb53b
mrrayaan123 <mohammedrayaan1@gmail.com>
2022-11-28 23:44:32 +0530

    Commited Javascript files

---
+++ hello.js
@@ -0,0 +1,7 @@
+// To Print Output
+
+console.log("Hello World");
+console.log(5+10);
+console.log(5*10);
+
+
---
+++ task1.js
@@ -0,0 +1,12 @@
+//To print op
+console.log("Name:Mohammed Riyaan");
+console.log("Age:21")
+console.log("Lords")
+
+//This is called single line comment.
+
+/*hello bois
+this is whole comment
+this is called multi line comment */
+
+//Semi colon is optional.
